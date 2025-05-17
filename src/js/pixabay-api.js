c78053b6033aa8053cd791acd31193b3972ffd78
Kateryna <tyutyunnikova.katia@gmail.com>
2025-05-17 22:32:42 +0300

    gallery

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_KEY = '50322611-1d4a2dda66e83b148172da45e';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch images from Pixabay');
+  }
+}
